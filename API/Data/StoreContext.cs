3535870d1b739da9bb75fce5dd75fe8ad0b6a545
beginner1986 <beginner1986@gmail.com>
2023-05-26 13:09:01 +0200

    DbContext and migration added

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class StoreContext
+    public class StoreContext : DbContext
     {
-        
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
