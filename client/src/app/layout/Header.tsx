687f2e5fed65b4c81df8eb30a8978475e1d9d2ad
beginner1986 <beginner1986@gmail.com>
2023-05-30 22:24:04 +0200

    Client: Header and Container for main part added

---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,11 @@
+import { AppBar, Typography } from "@mui/material";
+
+export default function Header() {
+    return (
+        <AppBar position="static">
+            <Typography variant="h6">
+                RE-STORE
+            </Typography>
+        </AppBar>
+    )
+}
