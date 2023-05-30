69d83c19474d71495151362d26c8bef44cafa0b3
beginner1986 <beginner1986@gmail.com>
2023-05-30 22:36:51 +0200

    Product cares placeholders added

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,7 +2,7 @@ import { AppBar, Typography } from "@mui/material";
 
 export default function Header() {
     return (
-        <AppBar position="static">
+        <AppBar position="static" sx={{mb: 4}}>
             <Typography variant="h6">
                 RE-STORE
             </Typography>
