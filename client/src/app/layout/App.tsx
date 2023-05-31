41df298f460d8935a4b0537733e6a2d946c240c9
beginner1986 <beginner1986@gmail.com>
2023-05-31 21:11:46 +0200

    Client: products state moved from App to Catalog

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,36 +1,14 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/Product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline, Typography } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then(response => response.json())
-      .then(data => setProducts(data));
-  }, []);
-
-  const addProduct = () => {
-    setProducts(prevState => [...prevState,
-      {
-        id: prevState.length + 101,
-        name: 'product ' + (prevState.length + 1),
-        brand: 'brand',
-        description: 'description',
-        pictureUrl: 'http://picsum.photos/200',
-        price: 100.00 + (prevState.length + 1)
-      }])
-  }
-
-  return (
+    return (
     <>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog />
       </Container>
     </>
   );
