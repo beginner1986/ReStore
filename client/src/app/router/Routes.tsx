210264c341de0dbda720a52b496306e9dbf51673
beginner1986 <beginner1986@gmail.com>
2023-06-01 22:28:12 +0200

    Client: NavLinks added to AppBar

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -11,11 +11,11 @@ export const router = createBrowserRouter([
         path: "/",
         element: <App />,
         children: [
-            { path: '', element: <HomePage />},
-            { path: 'catalog', element: <Catalog />},
-            { path: 'catalog/:id', element: <ProductDetails />},
-            { path: 'about', element: <AboutPage />},
-            { path: 'contact', element: <ContactPage />}
+            { path: '', element: <HomePage /> },
+            { path: 'catalog', element: <Catalog /> },
+            { path: 'catalog/:id', element: <ProductDetails /> },
+            { path: 'about', element: <AboutPage /> },
+            { path: 'contact', element: <ContactPage /> }
         ]
     },
 ])
