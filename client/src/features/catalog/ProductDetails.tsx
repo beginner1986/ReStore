e9cfc709267143e76a563eea6f857d9e468a5f01
beginner1986 <beginner1986@gmail.com>
2023-06-09 22:23:25 +0200

    Client: NotFound component implemented

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/Product";
 import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
 
 export default function ProductDetails() {
     const {id} = useParams<{id: string}>();
@@ -21,7 +22,7 @@ export default function ProductDetails() {
         return <h3>Loading...</h3>
 
     if(!product)
-        return <h3>Product not found</h3>
+        return <NotFound />
 
     return (
         <Grid container spacing={6}>
