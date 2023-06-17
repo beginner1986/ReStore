bfafa79d2ef0126dad9f46a538261ca0e751ce90
beginner1986 <beginner1986@gmail.com>
2023-06-17 20:29:53 +0200

    API: removing item from basket implemented

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -62,12 +62,19 @@ namespace API.Controllers
         }
 
         [HttpDelete]
-        public async Task<ActionResult> RemoveBasketItem(int productId, int wuantity)
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
         {
-            // get basket
-            // remove item or reduce quantuty
-            // save changes
-            return StatusCode(204);
+            var basket = await RetrieveBsket();
+
+            if (basket == null)
+                return NotFound();
+
+            basket.RemoveItem(productId, quantity);
+            var result = await _context.SaveChangesAsync() > 0;
+            if (result)
+                return Ok();
+
+            return BadRequest(new ProblemDetails { Title = " Probelm removing item from the basket" });
         }
 
         private async Task<Basket> RetrieveBsket()
