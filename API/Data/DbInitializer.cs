0fac1ee285f211d796d24f0dc41edab4cd21c3a7
beginner1986 <beginner1986@gmail.com>
2023-07-12 21:09:42 +0200

    API: entity configuration and migration added

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,12 +1,34 @@
+using System.Collections.Generic;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 namespace API.Data
 {
     public static class DbInitializer
     {
-        public static void Initialize(StoreContext context)
+        public static async Task Initialize(StoreContext context, UserManager<User> userManager)
         {
-            if(context.Products.Any()) 
+            if (!userManager.Users.Any())
+            {
+                var user = new User
+                {
+                    UserName = "bob",
+                    Email = "bob@test.com"
+                };
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
+
+                var admin = new User
+                {
+                    UserName = "admin",
+                    Email = "admin@test.com"
+                };
+                await userManager.CreateAsync(admin, "Pa$$w0rd");
+                await userManager.AddToRolesAsync(admin, new[] { "Member", "Admin" });
+            }
+
+            if (context.Products.Any())
                 return;
 
             var products = new List<Product>
@@ -208,7 +230,7 @@ namespace API.Data
                     QuantityInStock = 100
                 },
             };
-            
+
             foreach (var product in products)
             {
                 context.Products.Add(product);
